7732e52e1f983df60542e914388fb4cac35924f6
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-01-30 17:17:13 -0800

    Added age categories

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -4,13 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------ Academic Readiness Evaluator ------\n");
+            Console.WriteLine("------ Academic & Personal Readiness Evaluator ------\n");
 
             int currentYear = DateTime.Now.Year;
             Console.WriteLine($"Current Year: {currentYear}");
 
             Console.Write("Enter your birth year: ");
             int birthYear = int.Parse( Console.ReadLine() );
+            int age = currentYear - birthYear;
+
+
+            Console.WriteLine($"Calculated Age: {age}");
+
+            if ( age < 0)
+            {
+                Console.WriteLine("Readiness Category: Unknown due to invalid age");
+            }
+            else if ( age < 18)
+            {
+                Console.WriteLine("Readiness Category: Minor");
+            }
+            else
+            {
+                Console.WriteLine("Readiness Category: Adult");
+            }
+
 
 
         }
